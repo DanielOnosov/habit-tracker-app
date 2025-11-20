8b9394c82e7196b6415e0820870da68d8f081e46
Daniel Onosov <onosov20071@gmail.com>
2025-11-20 19:12:55 +0200

    Initial Commit

---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Danylo Onosov on 20.11.2025.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
